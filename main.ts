f162ea8607ae6810df823131919bdaed46dfcf00
CAPRISUNisAWESOMESAUCE <scossw28@stu.mwacademy.org>
2026-05-19 12:50:55 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    music.play(music.stringPlayable("C5 C G E - G - G ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
